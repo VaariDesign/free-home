a1569295719a96bccfe64e5b289c8a014a4d05b3
VaariDesign <84449958+VaariDesign@users.noreply.github.com>
2022-10-11 21:27:57 +0200

    User input seperate module

---
+++ input_data.py
@@ -0,0 +1,4 @@
+# User input data, different in each system
+user =  'Username' # Login data for free@home: Settings---> Local API ---> Username
+password = 'Password' # same as log in your free@home
+url = 'IP-Address'
