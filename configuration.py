30080706d5c5ab5140c92fc93b7ad34f09d4e247
VaariDesign <84449958+VaariDesign@users.noreply.github.com>
2022-11-20 13:36:43 +0100

    repaired update function

--- configuration.py
+++ configuration.py
@@ -5,6 +5,7 @@ import input_data
 from classes import Light, Heating, Shade, Weather
 
 
+
 def make_light(sysap, device, channel, displayname, inputchannel):
     value = package_json[sysap]['devices'][str(device)]['channels'][channel]['inputs'][inputchannel]["value"]
     name = str(package_json[sysap]['devices'][str(device)]['channels'][channel]['displayName'])
@@ -55,13 +56,18 @@ def update(light_obj, heating_obj, shades_obj, weather_obj):
     for shade in shades_obj:
         shade.position = package_json[shade.sysap]['devices'][str(shade.device)]['channels'][shade.channel]['outputs'][shade.output_pos]["value"]
         shade.angle = package_json[shade.sysap]['devices'][str(shade.device)]['channels'][shade.channel]['outputs'][shade.output_ang]["value"]
+        shade.buttonvaluepos.set(shade.position)
+        shade.buttonvalueang.set(shade.angle)
 
     for heat in heating_obj:
-        heat.target = package_json[heat.sysap]['devices'][str(heat.device)]['channels'][heat.channel]['outputs'][heat.outputchannel]["value"]
+        heat.target = package_json[heat.sysap]['devices'][str(heat.device)]['channels'][heat.channel]['inputs'][heat.inputchannel]["value"]
         heat.temperature = package_json[heat.sysap]['devices'][str(heat.device)]['channels'][heat.channel]['outputs'][heat.temperature_channel]["value"]
+
 
     for data in weather_obj:
         data.value = package_json[data.sysap]['devices'][str(data.device)]['channels'][data.channel]['outputs'][data.outputchannel]["value"]
+
+
 
 
 
